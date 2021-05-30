4600ec15bf92f365b2ed4ef19469c7b3423247e9
Alain Moratalla <realmoratalla@gmail.com>
2021-05-31 07:13:39 +0800

    added environment variable dependency
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import colors from "colors";
+require('dotenv').config();
 colors.enable()
 
 async function server(){
     const app = express();
     await require('./loaders').default({ expressApp: app})
-    app.listen({ port: 4010}, () => {
-        console.log(`Listening at port 4010`.cyan.underline)
+    app.listen({ port: process.env.PORT || 5000 }, () => {
+        console.log(`Listening at port ${process.env.PORT || 5000}`.cyan.underline)
     })
 }
 
